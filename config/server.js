6f6e36c2ddfefff667906dc74b3c2087198be48e
Mao <doryantilmant25@gmail.com>
2021-09-23 22:13:12 +0200

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1b8e84cb12cf152d0bb3ae436eff33b2'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b7660e13-85d5-4c41-b4a2-8f92cddf0c55'
+};
